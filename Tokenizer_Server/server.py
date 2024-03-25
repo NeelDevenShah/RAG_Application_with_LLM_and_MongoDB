e1d02d784ea64cd14281d9d2cea1ad9ca27168e0
Neel Shah <neeldevenshah@gmail.com>
2024-03-25 17:33:47 +0530

    Made some tech changes in serve, and removed new server code

--- Tokenizer_Server/server.py
+++ Tokenizer_Server/server.py
@@ -2,17 +2,12 @@ import flask
 from flask import request, jsonify
 from sentence_transformers import SentenceTransformer
 import json
-import socketserver
-import threading
 
 app = flask.Flask(__name__)
 
 # Enable CORS
 from flask_cors import CORS
 CORS(app)
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
 
 @app.route("/checking", methods=["GET"])
 def checking():
@@ -24,9 +19,8 @@ def preprocess_text():
     texts = []
     for key, value in data.items():
         texts.append(f"{key}: {value}")
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     embeddings = model.encode(texts)
-    print("Processed a request successfully")
+    print("Processed an request successfully")
     return jsonify({"embeddings": embeddings.tolist()})
 
 def convert_json_to_text(data):
@@ -37,9 +31,5 @@ def convert_json_to_text(data):
     return texts
 
 if __name__ == "__main__":
-    server = ThreadedTCPServer(('0.0.0.0', 5000), app)
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
-    print("Server started.")
-    server_thread.join()
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    app.run(host="0.0.0.0", port=5000)  # Change port if needed
