fbfdc4cd649586cfd37a6ead7d699b3f7ef27eaf
Neel Shah <neeldevenshah@gmail.com>
2024-03-25 16:24:54 +0530

    Enabled the multi threading in server

--- Tokenizer_Server/server.py
+++ Tokenizer_Server/server.py
@@ -2,12 +2,17 @@ import flask
 from flask import request, jsonify
 from sentence_transformers import SentenceTransformer
 import json
+import socketserver
+import threading
 
 app = flask.Flask(__name__)
 
 # Enable CORS
 from flask_cors import CORS
 CORS(app)
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
 @app.route("/checking", methods=["GET"])
 def checking():
@@ -21,7 +26,7 @@ def preprocess_text():
         texts.append(f"{key}: {value}")
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     embeddings = model.encode(texts)
-    print("Processed an request successfully")
+    print("Processed a request successfully")
     return jsonify({"embeddings": embeddings.tolist()})
 
 def convert_json_to_text(data):
@@ -32,4 +37,9 @@ def convert_json_to_text(data):
     return texts
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)  # Change port if needed
+    server = ThreadedTCPServer(('0.0.0.0', 5000), app)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    print("Server started.")
+    server_thread.join()
