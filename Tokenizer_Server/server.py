4b3c61e485889c2dca17b17456f48ecc6e80bd51
Neel Shah <neeldevenshah@gmail.com>
2024-03-26 21:56:56 +0530

    Added try catch facility

--- Tokenizer_Server/server.py
+++ Tokenizer_Server/server.py
@@ -9,18 +9,21 @@ CORS(app)
 @app.route("/checking", methods=["GET"])
 def checking():
     return "Working!!"
+
 @app.route("/preprocess", methods=["POST"])
 def preprocess_text():
-    data = request.get_json()
-    texts = []
-    for key, value in data.items():
-        texts.append(f"{key}: {value}")
-    print(texts)
-    print("-----------------------")
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-    embeddings = model.encode(texts)
-    print("Processed an request successfully")
-    return jsonify({"embeddings": embeddings.tolist()})
+    try:
+        data = request.get_json()
+        texts = []
+        for key, value in data.items():
+            texts.append(f"{key}: {value}")
+        print("-----------------------")
+        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        embeddings = model.encode(texts)
+        print("Processed a request successfully")
+        return jsonify({"embeddings": embeddings.tolist()})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 def convert_json_to_text(data):
     texts = []
@@ -28,5 +31,6 @@ def convert_json_to_text(data):
         text = item.get("text")
         texts.append(text)
     return texts
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)  # Change port if needed
